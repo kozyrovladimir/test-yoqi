e3af77c10dee1760c975c7f174a437b964b7bb5f
kozyrovladimir <kozyrovladimir@gmail.com>
2022-03-18 17:50:47 +0300

    create test template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Search from "./components/search";
+import AppBar from "./components/appBar";
+import {theme} from './components/appTheme';
+import Button from '@material-ui/core/Button';
+import { ThemeProvider } from '@mui/material';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <div className="App">
+                <AppBar />
+                <Search/>
+                <Button variant="contained">Contained</Button>
+            </div>
+        </ThemeProvider>
+    );
 }
 
 export default App;
---
+++ src/components/appTheme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from "@material-ui/core";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            light: '#fafafa',
+            main: '#fafafa',
+            dark: '#fafafa',
+            contrastText: '#000',
+        },
+        secondary: {
+            light: '#ff7961',
+            main: '#f44336',
+            dark: '#ba000d',
+            contrastText: '#000',
+        },
+    },
+});
